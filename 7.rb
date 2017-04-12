34d326006308b356767dd3b463d7cad028d20047
高谷大樹 <takayadaiki@takayadaiki-no-MacBook-Pro.local>
2017-04-12 15:58:20 +0900

    コメントの追加

--- 7.rb
+++ 7.rb
@@ -1,21 +1,27 @@
-
-def high_and_low
-	cp_answer = rand(1..100)
-	#p cp_answer
-	puts "0-100で数字を当てて"
-	your_answer = gets.to_i #コンソールから入力
+def judge(your_answer, cp_answer)
 	diff = cp_answer - your_answer #両者の差を見る
-
-	if diff == 0
+	if diff == 0 #差で条件分岐させる
 		p "正解です"
-	elsif diff > 0
+	elsif diff > 0 #正解以外はもう一度high_and_lowを呼ぶ
 		p "もっと上です"
 		high_and_low
 	else
 		p "もっと下です"
 		high_and_low
 	end
-
 end
+
+
+
+
+def high_and_low
+	puts "0-100で数字を当ててください"
+	cp_answer = rand(1..100) #答えを乱数で生成
+	#p cp_answer
+	your_answer = gets.to_i #ターミナルから入力を受け付ける
+	judge(your_answer, cp_answer) #条件分岐の箇所を呼び出す。
+end
+
+
 
 high_and_low
