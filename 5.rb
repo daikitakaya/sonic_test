64af96172e0e75658ac3197aab6c45d85f169b57
高谷大樹 <takayadaiki@takayadaiki-no-MacBook-Pro.local>
2017-04-12 15:49:47 +0900

    no message

--- 5.rb
+++ 5.rb
@@ -1,25 +1,40 @@
+#------------------------------------------
+#配列の合計を求めるメソッド
+
 def sum_array(array)
 	sum = 0
 	for ary_num in 0..array.length
 		sum = sum + array[ary_num].to_i
 	end
-	p sum
+ sum
 end
 
-sum_array([20, 31, 42, 13, 5, 38])#合計
+p sum_array([20, 31, 42, 13, 5, 38])#合計
+
+#------------------------------------------
 
 
+
+
+#------------------------------------------
+#平均を求めるメソッド、前回の結果を利用
 
 def average_number(array)
-	p sum_array(array) / array.length
+ (sum_array(array) / array.length).to_f
 end
 
-average_number([20, 31, 42, 13, 5, 38])#平均
+p average_number([20, 31, 42, 13, 5, 38])#平均
 
+#---------------------------------------------
+
+
+
+#---------------------------------------------
+#配列の最大値を求めるメソッド
 
 def max_in_array(array)
 	max_num = array[0]
-	count = array.length -1
+	count = array.length - 1
 	for i in 1..count
 		if array[i].to_i > max_num
 			max_num = array[i]
@@ -30,6 +45,11 @@ end
 
 max_in_array([20, 31, 42, 13, 5, 38])#最大値
 
+#----------------------------------------------
+
+
+#----------------------------------------------
+#配列の最小値を求めるメソッド
 
 def min_in_array(array)
 	min_num = array[0]
@@ -44,10 +64,14 @@ end
 
 min_in_array([20, 31, 42, 13, 5, 38])#最小
 
+#------------------------------------------------
 
 
 
-def bubble_sort(array)
+#-------------------------------------------------
+#配列を大きい順に並べる(バブルソート)
+
+def bubble_sort_des(array)
 	num1 = array.length
 	num2 = array.length - 1
 for i in 0..num2
@@ -57,27 +81,34 @@ for i in 0..num2
 		end
 	end
 end
-p array
+ array
 end
 
 
 
+p bubble_sort_des([20, 31, 42, 13, 5, 38])#大きい順
+
+#-----------------------------------------------------
 
 
-bubble_sort([20, 31, 42, 13, 5, 38])#大きい順
 
 
-def bubble_sort_deg(array)
+#-----------------------------------------------------
+#配列を小さい順に並べる（バブルソート）
+
+def bubble_sort_asc(array)
 	num1 = array.length
 	num2 = array.length - 1
-for i in 0..num1
-	for j in 0..num2
-		if array[j].to_i > array[j-1].to_i
+for i in 0..num2
+	for j in 1..num2
+		if array[j].to_i < array[j-1].to_i
 			array[j], array[j-1] = array[j-1], array[j]
 		end
 	end
 end
-p array
+ array
 end
 
-bubble_sort_deg([20, 31, 42, 13, 5, 38])#小さい順
+p bubble_sort_asc([20, 31, 42, 13, 5, 38])#小さい順
+
+#--------------------------------------------------------
